f76136dffe2bc119c95163425072dfc4b66f1240
iokl1004 <iokl3319@naver.com>
2021-11-23 17:33:52 +0900

    자바의 정석 (기초편)

---
+++ workspace/ch06/src/Ex6_2.java
@@ -0,0 +1,19 @@
+public class Ex6_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tv t1 = new Tv();
+		Tv t2 = new Tv();
+		System.out.println("t1의 channel 값은 " + t1.channel + "입니다.");
+		System.out.println("t2의 channel 값은 " + t2.channel + "입니다.");
+		
+		t1.channel = 7;	//channel 값을 7으로 한다.
+		System.out.println("t1의 channel값을 7로 변경하였습니다.");
+		
+		t2 = t1; //참조변수 t1의 값을 t2에 저장
+		System.out.println("t1의 channel값은 " + t1.channel + "입니다.");
+		System.out.println("t2의 channel값은 " + t2.channel + "입니다.");
+
+	}
+
+}
