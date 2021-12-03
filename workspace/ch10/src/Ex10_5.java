317a597027f2aa1eecbb4f00889d0d4410df2f8f
iokl1004 <iokl3319@naver.com>
2021-12-03 14:25:27 +0900

    자바의 정석(기초편)

--- workspace/ch10/src/Ex10_5.java
+++ workspace/ch10/src/Ex10_5.java
@@ -1,36 +1,47 @@
 import java.util.Calendar;
 
 public class Ex10_5 {
-	static int getDayDiff(String yyyymmdd1, String yyyymmdd2) {
-		int diff = 0;
-		try {
-			int year1 = Integer.parseInt(yyyymmdd1.substring(0, 4));
-			int month1 = Integer.parseInt(yyyymmdd1.substring(4, 6)) -1;
-			int day1 = Integer.parseInt(yyyymmdd1.substring(6, 8));
-			int year2 = Integer.parseInt(yyyymmdd2.substring(0, 4));
-			int month2 = Integer.parseInt(yyyymmdd2.substring(4, 6)) -1;
-			int day2 = Integer.parseInt(yyyymmdd2.substring(6, 8));
-			Calendar date1 = Calendar.getInstance();
-			Calendar date2 = Calendar.getInstance();
-//			System.out.println("date1 =" + date1);
-//			System.out.println("date2 =" + date2);
-			date1.clear();
-			date2.clear();
-			date1.set(year1, month1, day1);
-			date2.set(year2, month2, day2);
-//			System.out.println((date1.getTimeInMillis() - date2.getTimeInMillis()));
-			diff = (int) ((date1.getTimeInMillis() - date2.getTimeInMillis()) / (24 * 60 * 60 * 1000));
-		} catch ( Exception e) {
-			diff = 0;
-		}
-		return diff;
-	}
-	
-	
+
 	public static void main(String[] args) {
-		System.out.println(getDayDiff("20010103", "20010101"));
-		System.out.println(getDayDiff("20010103", "20010103"));
-		System.out.println(getDayDiff("20010103", "200103"));
+		if(args.length !=2) {
+			System.out.println("Usage : java Ex10_5 2019 9");
+			return;
+		}
+		int year = 2019;
+		int month = 10;
+		int START_DAY_OF_WEEK = 0;	// 1일의 요일
+		int END_DAY = 0;
+		
+		Calendar sDay = Calendar.getInstance();// 시작일
+		Calendar eDay = Calendar.getInstance();	//끝일
+		
+		// 월의 경우 0부터 11까지의 값을 가지므로 1을 빼주어야 한다.
+		// 예를들어, 2019년 11월 1일은 sDay.set(2019, 10, 1); 과 같이 해주어야한다.
+		sDay.set(year, 8, 1);	//2019.9.1
+		eDay.set(year, 9, 1);	//2019.10.1
+		
+		//다음달의 첫날(12월 1일)에서 하루를 빼면 현재달의 마지막 날(11월30)이다.
+		eDay.add(Calendar.DATE, -1);	//2019.09.30
+		
+		// 첫 번째 요일이 무슨 요일인지 알아낸다. 1일이 무슨 요일인지 알아낸다.
+		START_DAY_OF_WEEK = sDay.get(Calendar.DAY_OF_WEEK);
+		
+		// eDay에 지정된 날짜를 얻어온다.
+		END_DAY = eDay.get(Calendar.DATE);
+		
+		System.out.println("       " + year + "년" + month + "월");
+		System.out.println(" SU MO TU WE TH FR SA");
+		
+		// 해당 월의 1일이 어느 요일인지에 따라서 공백을 출력한다.
+		// 만약 1일이 수요일이라면 공백을 세 번 찍는다. (일요일부터 시작)
+		for(int i = 1; i < START_DAY_OF_WEEK;i++)
+			System.out.print("   ");
+		
+		for(int i=1, n=3; i<=31; i++, n++) {
+			System.out.print((i<10)? "  "+i : " "+i);
+			if(n%7==0) System.out.println();
+		}
+		 
 	}
 
 }
