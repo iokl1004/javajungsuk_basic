14d2cb9223adb7503a0900329f1e57357e2b545d
iokl1004 <iokl3319@naver.com>
2021-12-14 16:58:26 +0900

    자바의 정석(기초편)

--- workspace/ch14/src/Ex14_0.java
+++ workspace/ch14/src/Ex14_0.java
@@ -1,23 +1,17 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 class Ex14_0 {
 	public static void main(String[] args) {
-//		Object obj = (a, b) -> a > b ? a : b;	//람다식. 익명객체
-//		Myfunction2 f = new Myfunction2() {
-//			public int max(int a, int b) {	//로어바리딩 - 접근 제어자는 좁게 못 바꾼다.
-//				return a > b ? a : b;
-//			}
-//		};
+		// iterate(T seed, UnaryOperator f)	단항 연산자
+		Stream<Integer> intStream = Stream.iterate(0,  n -> n + 2);
+		intStream.limit(10).forEach(System.out::println);
 		
-		// 람다식(익명 객체)을 다루기 위한 참조변수의 타입은 함수형 인터페이스로 한다.
-		Myfunction2 f = (a, b) -> a > b ? a : b;	//람다식. 익명객체
-		
-		
-		int value = f.max(3, 5);	//함수형 인터페이스
-		System.out.println("value = " + value);
+		// generate(Supplier s) : 추가만 하는것 입력x, 출력O
+		Stream<Integer> oneStream = Stream.generate(() -> 1);
+		oneStream
+//		.limit(10)
+		.forEach(System.out::println);
 	}
-}
-
-@FunctionalInterface	//함수형 인터페이스는 단 하나의 추상 메서드만 가져야 함.
-interface Myfunction2 {
-//	public abstract int max(int a, int b);
-	int max(int a, int b);
 }
